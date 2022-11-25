f27445a76dd1228e93363a9c1adfda33c5b3e073
김나영 kim-nayoung <102217891+kimna0920@users.noreply.github.com>
2022-11-25 20:34:21 +0900

    Add files via upload

---
+++ 주식회사 흔한/index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HeunHam inc.</title>
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <script src="https://kit.fontawesome.com/a0fdef4e51.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <div class="inner">
+            <h1><a href="index.php">HEUNHAM</a></h1>
+            <ul id="gnb">
+                <li><a href="#">DEPARTMENT</a></li>
+                <li><a href="#">GALLERY</a></li>
+                <li><a href="#">YOUTUBE</a></li>
+                <li><a href="#">COMMUNITY</a></li>
+                <li><a href="#">LOCATION</a></li>
+            </ul>
+            <ul class="util">
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Help</a></li>
+                <li><a href="#">Login</a></li>
+                <li><a href="join_from.php">Join</a></li>
+                <li><a href="#">Sitemap</a></li>
+            </ul>
+        </div>
+    </header>
+    <figure>
+        <video src="img/visual.mp4" autoplay muted loop>
+        </video>
+        <div class="inner">
+            <h1>WEEKEND</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis hic quisquam numquam, consectetur dicta quis magni quia ipsam ipsum inventore. Autem excepturi ab, sed ex nobis, quasi maxime at cum.</p>
+            <a href="#">view detail</a>
+        </div>
+    </figure>
+    <section>
+        <div class="inner">
+            <h1>BUSINESS</h1>
+            <div class="wrap">
+                <article>
+                    <div class="pic">
+                        <img src="img/business01.jpg" alt="">
+                    </div>
+                    <h2><a href="">Programming</a></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, ullam.</p>
+                </article>
+                <article>
+                    <div class="pic">
+                        <img src="img/business02.jpg" alt="">
+                    </div>
+                    <h2><a href="">Education</a></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, ullam.</p>
+                </article>
+                <article>
+                    <div class="pic">
+                        <img src="img/business03.jpg" alt="">
+                    </div>
+                    <h2><a href="">Design</a></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, ullam.</p>
+                </article>
+                <article>
+                    <div class="pic">
+                        <img src="img/business04.jpg" alt="">
+                    </div>
+                    <h2><a href="">Marketing</a></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, ullam.</p>
+                </article>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="inner">
+            <h1>BUSINESS</h1>
+            <div class="wrap">
+                <article>
+                    <div class="pic">
+                        <img src="img/business01.jpg" alt="">
+                    </div>
+                    <h2><a href="">Programming</a></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, ullam.</p>
+                </article>
+                <article>
+                    <div class="pic">
+                        <img src="img/business02.jpg" alt="">
+                    </div>
+                    <h2><a href="">Education</a></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, ullam.</p>
+                </article>
+                <article>
+                    <div class="pic">
+                        <img src="img/business03.jpg" alt="">
+                    </div>
+                    <h2><a href="">Design</a></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, ullam.</p>
+                </article>
+                <article>
+                    <div class="pic">
+                        <img src="img/business04.jpg" alt="">
+                    </div>
+                    <h2><a href="">Marketing</a></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, ullam.</p>
+                </article>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="inner">
+            <h1>BUSINESS</h1>
+            <div class="wrap">
+                <article>
+                    <div class="pic">
+                        <img src="img/business01.jpg" alt="">
+                    </div>
+                    <h2><a href="">Programming</a></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, ullam.</p>
+                </article>
+                <article>
+                    <div class="pic">
+                        <img src="img/business02.jpg" alt="">
+                    </div>
+                    <h2><a href="">Education</a></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, ullam.</p>
+                </article>
+                <article>
+                    <div class="pic">
+                        <img src="img/business03.jpg" alt="">
+                    </div>
+                    <h2><a href="">Design</a></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, ullam.</p>
+                </article>
+                <article>
+                    <div class="pic">
+                        <img src="img/business04.jpg" alt="">
+                    </div>
+                    <h2><a href="">Marketing</a></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, ullam.</p>
+                </article>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div class="inner">
+            <div class="upper">
+                <h1>HEUNHAM</h1>
+                <ul>
+                    <li><a href="#">Policy</a></li>
+                    <li><a href="#">Terms</a></li>
+                    <li><a href="#">Family site</a></li>
+                    <li><a href="#">Sitemap</a></li>
+                </ul>
+            </div>
+            <div class="lower">
+                <address>
+                    울산광역시 남구 대공원로 299-1<br>
+                    TEL &#58; 052-273-8551 e&#45;mail &#58; admin@heunham.com
+                </address>
+                <p>
+                    2022 HEUNHAM &copy; copyright all right reserved.
+                </p>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
